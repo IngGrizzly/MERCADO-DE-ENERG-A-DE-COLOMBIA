196ddf7b4c1b93c66ac98492d8d1926f2d6e331b
IngGrizzly <28juan.kamilo@gmail.com>
2025-05-12 15:02:10 -0500

    Add files via upload

---
+++ EmpresaDeEnergia/src/empresadeenergia/EmpresaDeEnergia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package empresadeenergia;
+
+/**
+ *
+ * @author RSP-L13-LT-014
+ */
+public class EmpresaDeEnergia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
